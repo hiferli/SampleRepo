842ac944d9632991aafff446f349a5932b24000a
Hiferli <joshi.ishaan.2001@gmail.com>
2022-01-28 16:13:31 +0530

    Made the SamplePython as Table Maker

--- SamplePython.py
+++ SamplePython.py
@@ -1,12 +1,8 @@
-print("Hello World")
-print("WTF were you doing ???")
-print("This is used to check git diff")
+import string
 
-'''
-Random Bull Hit go:
-bsdifbuas djaw
-sbhsj hhfdj
-sdhfb; AddressFamilyjioabduf iajdhbdkffbahidhfa
-abdifuh answer_challengeadif uoawhdfadiasdhuf Uadbfu adif 
-ahud fiawefhau effectiveauh uwed fiaw\\
-'''
+
+a = input("Ente the number: ")
+
+for i in range(1 , 11):
+    print((int(a) * i))
+
